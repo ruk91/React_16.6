e7c5d098a91ced609b3bf3c34da8d05b454194a1
ruk91 <sajithkasn@gmail.com>
2019-01-07 14:20:48 +0530

    Updated React.lazy()

--- src/Components/myComponent.js
+++ src/Components/myComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => {
     return(
         <div>
-            Hi, I am loaded!!
+            Hi, I was load after the firstComponent!
         </div>
     )
 }
